c0eb12af27ad2f5a54454568c14b5d591211fb49
MostafaIn <mostafa.hazareh@integrify.fi>
2019-03-13 16:10:40 +0200

    the App get more responsive now

--- bmiScript.js
+++ bmiScript.js
@@ -10,7 +10,12 @@ const overweight = document.querySelector("#overweight");
 const normal = document.querySelector("#normal");
 const underweight = document.querySelector("#underweight");
 
-// const elementArr = [obese, overweight, normal, underweight];
+function colorChange() {
+  obese.style.color="white";
+  overweight.style.color = "white";
+  normal.style.color = "white";
+  underweight.style.color = "white";
+}
 
 // EventListener
 
@@ -18,14 +23,10 @@ form.addEventListener("submit", function(e) {
   const height100 = userHeight.value / 100;
   let bmi = userWeight.value / (height100 * height100);
   bmi = bmi.toFixed(1);
+  colorChange();
   if (bmi >= 30) {
     userResult.innerHTML = bmi;
     obese.style.color = "red";
-    // overweight.style.color = "white";
-    // normal.style.color = "white";
-    // underweight.style.color = "white";
-
-    obese.style.fontSize = "20px";
     picChange.setAttribute(
       "style",
       "background: url('./images/obese.jpg') no-repeat center/contain; background-color: #fdfdfd; width:100%;"
@@ -33,53 +34,28 @@ form.addEventListener("submit", function(e) {
   } else if (bmi >= 25 && bmi <= 29.9) {
     userResult.innerHTML = bmi;
     overweight.style.color = "#751d21";
-    // normal.style.color = "white";
-    // underweight.style.color = "white";
-    // obese.style.color = "white";
-    overweight.style.fontSize = "20px";
     picChange.setAttribute(
       "style",
       "background: url('./images/overweight.jpg') no-repeat center/contain; background-color: #fdfdfd; width:100%;"
     );
   } else if (bmi >= 18.5 && bmi <= 24.9) {
     userResult.innerHTML = bmi;
-
     normal.style.color = "green";
-    // underweight.style.color = "white";
-    // obese.style.color = "white";
-    // overweight.style.color = "white";
-    normal.style.fontSize = "20px";
     picChange.setAttribute(
       "style",
       "background: url('./images/normal.jpg') no-repeat center/contain; background-color: #fdfdfd; width:100%;"
     );
-  } else if (bmi <= 18) {
+  } else {
     userResult.innerHTML = bmi;
-
     underweight.style.color = "#e87a14";
-    // obese.style.color = "white";
-    // overweight.style.color = "white";
-    // normal.style.color = "white";
-    underweight.style.fontSize = "20px";
     picChange.setAttribute(
       "style",
       "background: url('./images/underweight.jpg') no-repeat center/contain; background-color: #fdfdfd; width:100%;"
     );
-  } else {
-    obese.style.color = "white";
-    overweight.style.color = "white";
-    normal.style.color = "white";
-    underweight.style.color = "white";
-    underweight.style.fontSize = "10px";
-    picChange.setAttribute(
-      "style",
-      "background: url('./images/default.jpg.jpg') no-repeat center/contain; background-color: #fdfdfd; width:100%;"
-    );
   }
-
   userWeight.value = "";
   userHeight.value = "";
   userWeight.focus();
-  
+
   e.preventDefault();
 });
